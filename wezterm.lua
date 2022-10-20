695ed0bd46cc5fc68812b3a2de41faab09af5d5b
Derek Sauer <dereksauer.ca@gmail.com>
2022-10-19 20:36:11 -0400

    Corrected Linux specific bugs and syntax issues.

--- wezterm.lua
+++ wezterm.lua
@@ -1,22 +1,22 @@
 local wezterm = require('wezterm')
 
 -- Use bash on linux and powershell on windows
-local default_term = '/usr/bin/bash'
-local default_args = '-l'
+local default_term = {'/usr/bin/bash'}
+local default_args = {'-l'}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    default_term = 'C:/Program Files/WindowsApps/Microsoft.PowerShell_7.2.6.0_x64__8wekyb3d8bbwe/pwsh.exe'
+    default_term = {'C:/Program Files/WindowsApps/Microsoft.PowerShell_7.2.6.0_x64__8wekyb3d8bbwe/pwsh.exe'}
     default_args = {'-NoLogo', '-NoProfile'}
 end
 
 -- My prefered font has a different name on Windows and Linux
-local default_font = 'Jetbrains Mono NF'
+local default_font = 'Jetbrains Mono Nerd Font'
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     default_font = 'JetBrainsMono NF'
 end
 
 -- Compact launch args
-local launch_args = {default_term}
-for index, value in pairs(default_args) do
+local launch_args = default_term
+for _, value in pairs(default_args) do
     table.insert(launch_args, value)
 end
 
@@ -57,6 +57,6 @@ return {
         },
         font_size = 10,
         active_titlebar_bg = '#1F1F28',
-        inactive_titlebar_bg = '#16161D'
+        inactive_titlebar_bg = '#1F1F28'
     }
 }
