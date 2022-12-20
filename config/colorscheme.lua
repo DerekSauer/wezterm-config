5d82a963deb4d491a52d21531a29725bd42ffd25
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-20 11:09:56 -0500

    Switched to ayu-mirage, regular was too dark.

--- config/colorscheme.lua
+++ config/colorscheme.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 -- Colorscheme
-M.colorscheme_preset = "ayu"
+M.colorscheme_preset = "Ayu Mirage"
 -- M.colorscheme_preset = "Catppuccin Mocha"
 M.colorscheme_table = wezterm.color.get_builtin_schemes()[M.colorscheme_preset]
 
