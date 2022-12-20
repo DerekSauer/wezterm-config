0566fc3c9ffa756e536aea81d8edc6b5cb58f1e7
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-20 07:44:47 -0500

    Giving the Ayu colorscheme a try.

--- config/colorscheme.lua
+++ config/colorscheme.lua
@@ -3,7 +3,8 @@ local wezterm = require("wezterm")
 local M = {}
 
 -- Colorscheme
-M.colorscheme_preset = "Catppuccin Mocha"
+M.colorscheme_preset = "ayu"
+-- M.colorscheme_preset = "Catppuccin Mocha"
 M.colorscheme_table = wezterm.color.get_builtin_schemes()[M.colorscheme_preset]
 
 return M
