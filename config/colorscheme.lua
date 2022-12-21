960be6864af14a3b087dcdd16d7e938c1cfb9699
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-21 15:19:49 -0500

    Revert back to catppuccin theme.

--- config/colorscheme.lua
+++ config/colorscheme.lua
@@ -3,8 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 -- Colorscheme
-M.colorscheme_preset = "Ayu Mirage"
--- M.colorscheme_preset = "Catppuccin Mocha"
+M.colorscheme_preset = "Catppuccin Macchiato"
 M.colorscheme_table = wezterm.color.get_builtin_schemes()[M.colorscheme_preset]
 
 return M
