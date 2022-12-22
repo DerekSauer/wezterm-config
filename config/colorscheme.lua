1f1c0613e49bc7c7ad0574f7ae382c43cfaf7e8b
Derek Sauer <dereksauer.ca@gmail.com>
2022-12-22 09:29:25 -0500

    Switched back to kanagawa now that my monitor gamma is fixed.

--- config/colorscheme.lua
+++ config/colorscheme.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 -- Colorscheme
-M.colorscheme_preset = "Catppuccin Macchiato"
+M.colorscheme_preset = "kanagawabones"
 M.colorscheme_table = wezterm.color.get_builtin_schemes()[M.colorscheme_preset]
 
 return M
