255ac53fa655330ec0a9f0412eab0c22ae72f542
Derek Sauer <dereksauer.ca@gmail.com>
2023-01-22 16:21:17 -0500

    Reference a builtin scheme. Easier to change schemes when I'm bored.

--- config/colorscheme.lua
+++ config/colorscheme.lua
@@ -1,31 +1,29 @@
+local colors = require("wezterm").get_builtin_color_schemes()["kanagawabones"]
+
 return {
-    foreground = "#dcd7ba",
-    background = "#1f1f28",
+    foreground = colors.foreground,
+    background = colors.background,
 
-    cursor_bg = "#c8c093",
-    cursor_fg = "#c8c093",
-    cursor_border = "#c8c093",
+    cursor_bg = colors.cursor_bg,
+    cursor_fg = colors.cursor_fg,
+    cursor_border = colors.cursor_border,
 
-    selection_fg = "#c8c093",
-    selection_bg = "#2d4f67",
+    selection_fg = colors.selection_fg,
+    selection_bg = colors.selection_bg,
 
-    scrollbar_thumb = "#16161d",
-    split = "#16161d",
-
-    ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-    brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+    ansi = colors.ansi,
+    brights = colors.brights,
 
     tab_bar = {
-        background = "#1f1f28",
+        background = colors.background,
         new_tab = {
-            bg_color = "#1f1f28",
-            fg_color = "#dcd7ba",
+            bg_color = colors.background,
+            fg_color = colors.foreground,
             intensity = "Bold",
         },
         new_tab_hover = {
-            bg_color = "#1f1f28",
-            fg_color = "#dcd7ba",
+            bg_color = colors.background,
+            fg_color = colors.foreground,
             italic = true,
         },
     },
